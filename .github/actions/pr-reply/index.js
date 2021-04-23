1029f93ab66eb240ad02b7436bd8a40d0ac45fbc
Ansuman Behera <71064674+AB-san@users.noreply.github.com>
2021-04-23 19:19:18 +0530

    Update index.js

--- .github/actions/pr-reply/index.js
+++ .github/actions/pr-reply/index.js
@@ -8,10 +8,13 @@ const octokit = github.getOctokit(GITHUB_TOKEN);
 const { context = {} } = github;
 const { pull_request } = context.payload;
 
-await octokit.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
-  });
+async function run(){
+    await octokit.issues.createComment({
+     ...context.repo,
+     issue_number: pull_request.number,
+     body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+    });
+}
 
+run();
   
